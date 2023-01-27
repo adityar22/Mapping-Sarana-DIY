4d73bb06010936d1fc442113399a92285b154ae9
adityar22 <adityaramadhanarckarito47@gmail.com>
2023-01-27 16:27:09 +0700

    sync login

--- frontend/src/components/modal/addFacility.jsx
+++ frontend/src/components/modal/addFacility.jsx
@@ -53,7 +53,7 @@ const AddFacility = ({ url, category, selfPopUp, chooseCatPopUp, addButtonVisibl
 
     return (
         <>
-            <div className="overlay z-100"></div>
+            {/* <div className="overlay z-100"></div> */}
             <div className="container w-fit mx-auto">
                 <form className="w-screen max-w-xl mx-8 bg-white shadow-xl rounded-3xl px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
                     <div className="flex justify-between">
