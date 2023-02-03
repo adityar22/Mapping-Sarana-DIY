983e8954e8a7c3ee009383bf6f158f3418d7b952
agnessuarna <agnessuarna@mail.ugm.ac.id>
2023-02-03 13:03:41 +0700

    update

--- frontend/src/components/modal/addCategory.jsx
+++ frontend/src/components/modal/addCategory.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useCategoryContext } from "../../hooks/useCategoryContext";
+import { useCategoryContext } from "../../hooks/usecategoryContext";
 import { useCategoryHandleAdd } from "../../hooks/useCategoryHandleAdd";
 import ChooseIcon from "./chooseIcon";
 
