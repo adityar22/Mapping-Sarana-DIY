5fb6785632741a0de26aeb6a8cabf8e74d30b2e3
AriqAryasuta <ariqaryasuta3@gmail.com>
2023-02-28 10:11:01 +0700

    Fixing refresh Issue

--- frontend/src/pages/mainpage.js
+++ frontend/src/pages/mainpage.js
@@ -1,4 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from 'axios'
+import jwt_decoded from 'jwt-decode'
 
 import Navbar from "../components/public/Navbar";
 import Map from "./map";
@@ -6,6 +9,24 @@ import SearchBar from "../components/searchbar/searchbar";
 import LocationList from "./LocationList";
 
 const Mainpage = () => {
+  // const [name, setName] = useState('');
+  // const [token, setToken] = useState('');
+
+  // useEffect(() => {
+  //   refreshToken();
+  // }, [])
+
+  // const refreshToken = async () => {
+  //   try {
+  //     const response = await axios.get('http://localhost:5000/account/token')
+  //     setToken(response.data.accessToken);
+  //     const decoded = jwt_decoded(response.data.accessToken)
+  //   } catch (error) {
+
+  //   }
+  // }
+
+
   return (
     <div className="max-w-screen flex-row sm:flex h-screen">
       <Navbar />
