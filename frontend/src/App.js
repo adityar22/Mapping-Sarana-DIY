2f3da2600eca380eaef9a940ba8774c4f8c744c3
AriqAryasuta <ariqaryasuta3@gmail.com>
2023-02-28 19:06:32 +0700

    Fixing Refresh Issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { useAuthContext } from "./hooks/useAuthContext";
 
+import { useLogin } from "./hooks/useLogin";
 import LocationList from "./pages/LocationList";
 import Map from "./pages/map";
 import { ToastContainer } from "react-toastify";
@@ -11,20 +13,31 @@ import { Login } from "./pages/login";
 import Mainpage from "./pages/mainpage";
 
 function App() {
-  const { user } = useAuthContext();
+
+  const { user, dispatch } = useAuthContext();
+  const [isLoading, setLoading] = useState(true)
+  useEffect(() => {
+    const logged = localStorage.getItem('user');
+    dispatch({ type: 'LOGIN', payload: logged });
+    setLoading(false);
+  }, [])
   console.log(user);
   return (
     <>
       <ToastContainer />
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/*"
-            element={!user ? <Login /> : <Mainpage />}
-          />
-        </Routes>
-      </Router>
+      <div className="max-w-screen flex-row sm:flex">
+        <div className="sm:overflow-y-scroll relative w-screen">
+          <Router>
+            <Routes>
+              <Route
+                exact
+                path="/*"
+                element={!user ? <Login /> : <Mainpage />}
+              />
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </>
   );
 }
