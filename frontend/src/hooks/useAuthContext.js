bff47a27374654db09fd16dfe47d2e55ac8481d2
AriqAryasuta <ariqaryasuta3@gmail.com>
2023-02-28 18:30:03 +0700

    Removing Dependencies

--- frontend/src/hooks/useAuthContext.js
+++ frontend/src/hooks/useAuthContext.js
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 
-export const useAuthContext=()=>{
+export const useAuthContext = () => {
     const context = useContext(AuthContext)
 
-    if(!context){
+
+    if (!context) {
         throw Error('useAuthContext must be used inside an AuthContextProvider')
     }
 
